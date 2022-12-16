18167dabdd58fce57fd9a7b7e365b92c53a1129e
dte3d <ndressler@dressler-tech.ca>
2022-12-16 14:37:47 +0000

    Initial commit of models

---
+++ models/PIX301_inbound_48.sql
@@ -0,0 +1,18 @@
+final as (
+
+	select DISTINCT 
+	d_m48.id,
+	d_m48.processed,
+	d_mm48.received_date,
+	d_mcm48.client_id,
+	d_mm48.response_date,
+	d_mm48.connector_name, 
+	d_mm48.error_code
+    from d_m48
+    left join d_mm48
+	on d_m48.id=d_mm48.message_id
+    left join d_mcm48
+	on d_m48.id=d_mcm48.message_id
+)
+
+select * from final
