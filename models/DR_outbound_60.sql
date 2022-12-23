bc9b27cce40b5ec086a431d4ca6f3d7366089fc9
Cub69 <norm@dressler.ca>
2022-12-23 10:41:48 -0500

    Added DR Outbound to Velatura

---
+++ models/DR_outbound_60.sql
@@ -0,0 +1,45 @@
+with d_m60 as (
+	select
+		id,
+		processed
+	from mirth_dw.public.d_m60
+
+),
+
+d_mcm60 as (
+	select
+		message_id,
+		doc_errored,
+		doc_returned
+	from mirth_dw.public.d_mcm60
+),
+
+d_mm60 as (
+	select
+		message_id,
+		received_date,
+		response_date,
+		connector_name,
+		error_code
+	from mirth_dw.public.d_mm60
+),
+
+final as (
+
+	select  
+	d_m60.id,
+	d_m60.processed,
+	d_mm60.received_date,
+	d_mcm60.doc_errored,
+	d_mcm60.doc_returned, 
+	d_mm60.response_date,
+	d_mm60.connector_name, 
+	d_mm60.error_code
+    from d_m60
+    left join d_mm60
+	on d_m60.id=d_mm60.message_id
+    left join d_mcm60
+	on d_m60.id=d_mcm60.message_id
+)
+
+select  distinct * from final order by id
